e1ba8889d69b0e8be3b3a98b8e14fd01f83cf61a
hannehue <hannehue@gmail.com>
2021-08-22 14:45:30 +0200

    Correct square function prop

--- src/index.js
+++ src/index.js
@@ -2,19 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
 
-class Square extends React.Component {
-
-  render() {
-    return (
-      <button 
-      className="square" 
-      onClick={() => this.props.onClick()}>
-        {this.props.value}
-      </button>
-    );
-  }
-}
-
 function Square(props) {
   return (
     <button className="square" onClick={props.onClick}>
@@ -22,6 +9,14 @@ function Square(props) {
     </button>
   )
 }
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
